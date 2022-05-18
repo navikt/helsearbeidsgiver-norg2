e8aec63b3e57adb6d29287128149b4534e931933
Morten Laukvik <morten.laukvik@nav.no>
2022-05-18 15:47:18 +0200

    Versjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val mockk_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val mockkVersion: String by project
 val githubPassword: String by project
 
 plugins {
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.serialization") version "1.6.21"
+    kotlin("jvm")
+    kotlin("plugin.serialization")
     id("org.jmailen.kotlinter") version "3.10.0"
     id("maven-publish")
 }
@@ -56,12 +56,11 @@ publishing {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-json:$ktor_version")
-    implementation("io.ktor:ktor-client-serialization:$ktor_version")
-    testImplementation("io.ktor:ktor-client-mock:$ktor_version")
-    implementation("com.nimbusds:nimbus-jose-jwt:9.22")
-    implementation("no.nav.security:token-client-core:2.0.15")
-    implementation("no.nav.helsearbeidsgiver:helsearbeidsgiver-tokenprovider:0.1.2")
-    testImplementation("io.mockk:mockk:$mockk_version")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
+    implementation("com.nimbusds:nimbus-jose-jwt:9.+")
+    implementation("no.nav.helsearbeidsgiver:helsearbeidsgiver-tokenprovider:0.+")
+    testImplementation("io.mockk:mockk:$mockkVersion")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
 rootProject.name = "norg2-client"
+
+pluginManagement {
+    val jvmVersion: String by settings
+    plugins {
+        kotlin("jvm") version jvmVersion
+        kotlin("plugin.serialization") version jvmVersion
+        id("org.jmailen.kotlinter")
+        id("maven-publish")
+    }
+}
